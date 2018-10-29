2ac4952b601ea81db93ad5ad24681a75ec41d719
HongZicong <973138379@qq.com>
2018-10-29 19:39:59 +0800

    update framework

--- block.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 29 17:40:07 2018
-
-@author: Dv00
-"""
-
--- cipher.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Oct 29 17:41:02 2018
-
-@author: Dv00
-"""
-
